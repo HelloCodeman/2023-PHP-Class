ca1653968d79df2055a54391e0c51ecfc5f10239
HelloCodeman <955khm@gmail.com>
2023-10-13 13:22:11 +0800

    直角三角形

---
+++ 20231013/practice3.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+echo "*<br>";
+     echo "*";
+     echo "<br>";
+echo "**<br>";
+     echo "*";
+     echo "*";
+     echo "<br>";
+echo "***<br>";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "****<br>";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "*****<br>";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "<hr>";
+
+$i  $j
+1   1
+2   2
+3   3
+4   4
+5   5
+*/
+for($i=0;$i<5;$i++){
+    for($j=0;$j<=$i;$j++){
+        echo "*";
+    }
+    echo "<br>";
+
+}
+
+?>
