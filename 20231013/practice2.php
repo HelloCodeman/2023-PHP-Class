beab47a4b279d40e19386611bebdcafd0a8e1369
HelloCodeman <955khm@gmail.com>
2023-10-13 13:07:52 +0800

    尋找位置

--- 20231013/practice2.php
+++ 20231013/practice2.php
@@ -3,8 +3,24 @@
 $source="印出尋找到的單字或字母所在句子中的位置";
 $target="字母";
 
+$flag="沒找到";
+$start=0;
+while($flag=="沒找到"){
 
+$tmp=mb_substr($source,$start,mb_strlen($target));
+if($tmp==$target){
+    $flag="找到";
+    break;
+}
+$start++;
+// $pos++;
 
+}
 
+echo "找到了;位置在".$start;
+echo "<BR>";
+echo "原始字串-$source";
+echo "<BR>";
+echo "尋找目標-$target";
 
 ?>
