687183cae6f5bbf14af119ced40f54a877ef2b17
HelloCodeman <955khm@gmail.com>
2023-10-13 15:44:57 +0800

    矩形

--- 20231013/test.php
+++ 20231013/test.php
@@ -1,21 +1,72 @@
-<?php
-echo "<h3>增加外框並直接顯示結果且有標頭的99乘法表</h3>";
-
-echo "<table border='1'>";
-echo "<tr>";
-echo "<th>x</th>";
-for ($i = 1; $i <= 9; $i++) {
-    echo "<th>" . $i . "</th>";
-}
-echo "</tr>";
-
-for ($j = 1; $j <= 9; $j++) {
-    echo "<tr>";
-    echo "<th>" . $j . "</th>";
-    for ($i = 1; $i <= 9; $i++) {
-        echo "<td>" . ($i * $j) . "</td>";
+<style>
+    *{
+        font-family:'Courier New';
     }
-    echo "</tr>";
+</style>
+<?php
+
+
+echo "<h2>直角三角形</h2>";
+
+for ($i = 0; $i < 5; $i++) {
+    for ($j = 0; $j <= $i; $j++) {
+        echo "*";
+    }
+    echo "<br>";
 }
-echo "</table>";
+
+echo "<h2>倒直角三角形</h2>";
+
+for ($i = 4; $i >= 0; $i--) {
+    for ($j = 0; $j <= $i; $j++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+echo "<h2>倒正三角形</h2>";
+
+for ($i = 4; $i >= 0; $i--) {
+    for ($j = 0; $j < (4 - $i); $j++) {
+        echo "&nbsp;";
+    }
+    for ($k = 0; $k < ($i * 2 + 1); $k++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+echo "<h2>菱形</h2>";
+
+for ($i = 0; $i < 9; $i++) {
+    if ($i <= 4) {
+        $tmp = $i;
+    } else {
+        $tmp--;
+    }
+    for ($j = 0; $j < (4 - $tmp); $j++) {
+        echo "&nbsp;";
+    }
+    for ($k = 0; $k < ($tmp * 2 + 1); $k++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+echo "<h2>矩形</h2>";
+
+for ($i = 0; $i < 7; $i++) {
+    for ($j = 0; $j < 7; $j++) {
+        if ($i == 0 || $i == 6 || $j == 0 || $j == 6) {
+            echo "*";
+        } else {
+            echo "&nbsp;";
+        }
+    }
+    echo "<br>";
+}
+
 ?>
+
+
+<?php
