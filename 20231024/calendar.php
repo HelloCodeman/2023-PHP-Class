79dd4f3698e39b9be8eba1253b301825a5087b25
HelloCodeman <955khm@gmail.com>
2023-10-24 11:44:02 +0800

    20231018modified

---
+++ 20231024/calendar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar</title>
+    <style>
+       container{
+            background-color: antiquewhite;
+            width: 300px;
+            height: 150px;
+            border: 2px solid;
+            overflow: visible;
+}
+        
+table,tr,td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            text-align: center;
+        }
+
+    </style>
+</head>
+<body>
+<div class="container">
+<table>
+            <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Mobile</td>
+                <td>Picture</td>
+                <td>Other</td>
+            </tr>
+
+
+
+
+    
+</body>
+</html>
