e7b01027f401793ecac171d06518b9ffc9334302
HelloCodeman <955khm@gmail.com>
2023-10-27 13:19:36 +0800

    完成BMI計算及頁面間傳值練習

--- 20231027/bmi.php
+++ 20231027/bmi.php
@@ -1,28 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>計算BMI</title>
 </head>
+
 <body>
     <h1>計算BMI</h1>
-    <form action="calc.php" method="get">
-        <div>
-            <label for="height">身高(m):</label>
-            <input type="text" name="height" id="height">
-        </div>
-        <div>
-            <label for="weight">體重(kg):</label>
-            <input type="text" name="weight" id="weight">
-        </div>
-        <div>
-            <input type="submit" value="計算BMI">
-            <input type="reset" value="重置">
-        </div>
+    <?php
+    if (!isset($_GET['bmi'])) {
 
-       <!-- 體重(kg)/身高(m)的平方 -->
+        if (isset($_GET['m'])) {
+            echo "<span style='color:red'>" . $_GET['m'] . "</span>";
+        }
+        ?>
 
-    </form>
+        <form action="calc.php" method="get">
+            <div>
+                <label for="height">身高(m):</label>
+                <input type="text" name="height" id="height">
+            </div>
+            <div>
+                <label for="weight">體重(kg):</label>
+                <input type="text" name="weight" id="weight">
+            </div>
+            <div>
+                <input type="submit" value="計算BMI">
+                <input type="reset" value="重置">
+            </div>
+
+            <!-- 體重(kg)/身高(m)的平方 -->
+
+        </form>
+
+        <?php
+    } else {
+        echo "身高:" . $_GET['h'] . "公尺";
+        echo "<BR>";
+        echo "體重:" . $_GET['w'] . "公斤";
+        echo "<BR>";
+        echo "BMI:" . $_GET['bmi'];
+        echo "<BR>";
+
+    }
+    ?>
 </body>
+
 </html>
--- 20231027/calc.php
+++ 20231027/calc.php
@@ -1,10 +1,25 @@
 <?php
-$weight=$_GET['weight'];
-$height=$_GET['height'];
 
-echo "身高:".$height;
-echo "體重:".$weight;
-echo "身高:".$_GET['weight'];
-echo "體重:".$_GET['height'];
+if (!empty($_GET)) {
+
+    if ($_GET['weight'] != "" && $_GET['height'] != "") {
+
+        $height = $_GET['height'];
+        $weight = $_GET['weight'];
+
+        $bmi = round($weight / ($height * $height), 2);
+        /*  echo "身高:" . $height;
+          echo "<br>";
+          echo "體重:" . $weight;
+          echo "<br>";
+          echo "BMI:" . $bmi; */
+
+        header("location:bmi.php?w=$weight&h=$height&bmi=$bmi");
+    } else {
+        header("location:bmi.php?m=請輸入身高或體重資訊");
+    }
+} else {
+    header("location:bmi.php?m=請輸入必要資訊");
+}
 
 ?>
