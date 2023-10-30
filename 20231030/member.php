ef270a61541028c893241a1366e9ec2571b0f7ec
HelloCodeman <955khm@gmail.com>
2023-10-30 14:19:16 +0800

    介紹include require的使用

--- 20231030/member.php
+++ 20231030/member.php
@@ -5,21 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>會員中心</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <?php
-
+    include_once "header.php";
+    ?>
+    <?php
     if (isset($_COOKIE['login']) && !empty($_COOKIE['login'])) {
         echo "<h3>登入成功</h3>";
         echo "<a href='login.php'>回登入頁</a>";
         echo "<br>";
         echo "<a href='logout.php'>登出</a>";
     } else {
-        setcookie("error","沒有登入相關驗證，非法登入",time()+5);
+        setcookie("error", "沒有登入相關驗證，非法登入", time() + 5);
         header("location:login.php");
     }
 
     ?>
 </body>
+
 </html>
