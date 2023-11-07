5c1461b8f5fe2c74ced4a470d08d38017ecbdce8
HelloCodeman <955khm@gmail.com>
2023-11-07 15:21:29 +0800

    萬年曆作業4

--- 20231024/calendar/index.php
+++ 20231024/calendar/index.php
@@ -4,133 +4,128 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>2024月曆</title>
+    <title>線上月曆</title>
     <style>
-        body {
-            text-align: center;
-            background-color: #f7f7f7;
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-        }
-
         .navbar {
             background-color: #333;
-            /* 背景颜色 */
+            /* 背景顏色 */
             color: #fff;
-            /* 文字颜色 */
+            /* 文字顏色 */
             padding: 10px;
-            /* 内边距 */
+            /* 內邊距 */
             display: flex;
             justify-content: space-between;
-            /* 将链接放在导航栏两侧 */
+            /* 將連結放在Navbar兩側 */
             align-items: center;
-            /* 垂直居中 */
+            /* 垂直置中 */
         }
 
         .navbar a {
             text-decoration: none;
-            /* 去掉链接下划线 */
+            /* 去掉連結底線 */
             color: #fff;
-            /* 链接颜色 */
+            /* 連結顏色 */
             margin: 0 10px;
-            /* 链接之间的间距 */
+            /* 連結之間的間距 */
         }
 
         .navbar a:hover {
             text-decoration: underline;
-            /* 鼠标悬停时显示下划线 */
         }
 
         table {
             border-collapse: collapse;
-            border: 3px double #999;
-            margin: 20px auto;
+            margin: 0 auto;
             background-color: #fff;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 1px;
         }
 
         td {
             border: 1px solid #999;
-            padding: 35px 75px;
+            padding: 30px;
             text-align: center;
-            font-size: 38px;
-        }
-
-        ul {
-            list-style-type: none;
-            padding: 0;
-            margin: 5px;
-
-        }
-
-        ul li {
-            display: inline-block;
-            font-size: 14px;
+            font-size: 28px;
+            border-radius: 15px;
+            box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
         }
     </style>
 </head>
 
 <body>
-
-    <div class="navbar">
-        <a href="index.php?year=2024&month=10">上一個月</a>
-        <h3>
-            <?php
-            $currentYear = date("Y");
-            $currentMonth = date("m");
-            echo "西元" . $currentYear . "年" . $currentMonth . "月";
-            ?>
-        </h3>
-        <a href="index.php?year=2024&month=12">下一個月</a>
-    </div>
-
     <?php
-    $thisMonth = date("Y");
-    $thisFirstDay = date("Y-m-1");
+
+    if (isset($_GET['month']) && isset($_GET['year'])) {
+        $month = $_GET['month'];
+        $year = $_GET['year'];
+    } else {
+        $month = date('m');
+        $year = date("Y");
+    }
+
+    echo "<h3 style='text-align:center'>";
+    echo date("西元{$year}年{$month}月");
+    echo "</h3>";
+    $thisFirstDay = date("{$year}-{$month}-1");
     $thisFirstDate = date('w', strtotime($thisFirstDay));
     $thisMonthDays = date("t");
-    $thisLastDay = date("Y-m-$thisMonthDays");
+    $thisLastDay = date("{$year}-{$month}-$thisMonthDays");
     $weeks = ceil(($thisMonthDays + $thisFirstDate) / 7);
     $firstCell = date("Y-m-d", strtotime("-$thisFirstDate days", strtotime($thisFirstDay)));
-    echo "<table>";
-    echo "<tr>";
-    echo "<td>日</td>";
-    echo "<td>一</td>";
-    echo "<td>二</td>";
-    echo "<td>三</td>";
-    echo "<td>四</td>";
-    echo "<td>五</td>";
-    echo "<td>六</td>";
-    echo "</tr>";
-    for ($i = 0; $i < $weeks; $i++) {
-        echo "<tr>";
-        for ($j = 0; $j < 7; $j++) {
-            $addDays = 7 * $i + $j;
-            $thisCellDate = strtotime("+$addDays days", strtotime($firstCell));
-            if (
-                date('w', $thisCellDate) == 0 ||
-                date('w', $thisCellDate) == 6
-            ) {
-                echo "<td style='background:pink'>";
-            } else {
-                echo "<td>";
-            }
-            if (
-                date(
-                    "m",
-                    $thisCellDate
-                ) == date("m", strtotime($thisFirstDay))
-            ) {
-                echo date("j", $thisCellDate);
-            }
-            echo "</td>";
-        }
-        echo "</tr>";
-    }
-    echo "</table>";
-
     ?>
+    <div class="navbar">
+        <?php
+        $nextYear = $year;
+        $prevYear = $year;
+        if (($month + 1) > 12) {
+            $next = 1;
+            $nextYear = $year + 1;
+        } else {
+            $next = $month + 1;
+        }
+
+        if (($month - 1) < 1) {
+            $prev = 12;
+            $prevYear = $year - 1;
+        } else {
+            $prev = $month - 1;
+        }
+
+        ?>
+        <a href="?year=<?= $prevYear; ?>&month=<?= $prev; ?>">上一個月</a>
+        <a href="?year=<?= $nextYear; ?>&month=<?= $next; ?>">下一個月</a>
+    </div>
+    <br>
+    <br>
+    <table>
+        <tr>
+            <td>日</td>
+            <td>一</td>
+            <td>二</td>
+            <td>三</td>
+            <td>四</td>
+            <td>五</td>
+            <td>六</td>
+        </tr>
+        <?php
+        for ($i = 0; $i < $weeks; $i++) {
+            echo "<tr>";
+            for ($j = 0; $j < 7; $j++) {
+                $addDays = 7 * $i + $j;
+                $thisCellDate = strtotime("+$addDays days", strtotime($firstCell));
+                if (date('w', $thisCellDate) == 0 || date('w', $thisCellDate) == 6) {
+                    echo "<td style='background:pink'>";
+                } else {
+                    echo "<td>";
+                }
+                if (date("m", $thisCellDate) == date("m", strtotime($thisFirstDay))) {
+                    echo date("j", $thisCellDate);
+                }
+                echo "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+        ?>
 
 </body>
 
--- 20231024/calendar/test.php
+++ 20231024/calendar/test.php
@@ -1,136 +1,144 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>2024月曆</title>
+    <title>線上月曆</title>
     <style>
-        body {
-            text-align: center;
-            background-color: #f7f7f7; /* 背景色 */
-            font-family: Arial, sans-serif; /* 字體 */
-            margin: 0;
-            padding: 0;
+        .navbar {
+            background-color: #333;
+            /* 背景颜色 */
+            color: #fff;
+            /* 文字颜色 */
+            padding: 10px;
+            /* 内边距 */
+            display: flex;
+            justify-content: space-between;
+            /* 将链接放在导航栏两侧 */
+            align-items: center;
+            /* 垂直居中 */
         }
-        
+
+        .navbar a {
+            text-decoration: none;
+            /* 去掉链接下划线 */
+            color: #fff;
+            /* 链接颜色 */
+            margin: 0 10px;
+            /* 链接之间的间距 */
+        }
+
+        .navbar a:hover {
+            text-decoration: underline;
+            /* 鼠标悬停时显示下划线 */
+        }
+
         table {
             border-collapse: collapse;
-            border: 3px double #999;
-            margin: 20px auto; /* 調整邊距 */
-            background-color: #fff; /* 表格背景色 */
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* 表格陰影效果 */
+            margin: 0 auto;
+            background-color: #fff;
+            
         }
 
         td {
-            border: 1px solid #999;
-            padding: 10px 20px;
+            border: 5px solid #999;
+            padding: 30px;
             text-align: center;
-            font-size: 18px;
+            font-size: 28px;
+           
         }
 
         ul {
             list-style-type: none;
             padding: 0;
-            margin: 20px;
+            margin: 5px;
+
         }
 
         ul li {
             display: inline-block;
-            font-size: 18px;
-            margin-right: 20px;
-        }
-
-        h3 {
-            background-color: #3498db; /* 標題背景色 */
-            color: #fff; /* 標題文字顏色 */
-            padding: 10px;
-            font-size: 24px;
+            font-size: 14px;
         }
     </style>
 </head>
+
 <body>
-<div style='width:264px;display:flex;margin:auto;justify-content:space-between'>
-<?php
-$year = date('Y');
-$month = date('n');
+    <?php
 
-$nextYear = $year;
-$prevYear = $year;
-
-if (($month + 1) > 12) {
-    $next = 1;
-    $nextYear = $year + 1;
-} else {
-    $next = $month + 1;
-}
-
-if (($month - 1) < 1) {
-    $prev = 12;
-    $prevYear = $year - 1;
-} else {
-    $prev = $month - 1;
-}
-
-?>
-    <a href="?year=<?=$prevYear;?>&month=<?=$prev;?>">上一個月</a>
-    <a href="?year=<?=$nextYear;?>&month=<?=$next;?>">下一個月</a>
-</div>
-
-<ul>
-    <li><a href="01.php">一月</a></li>
-    <li><a href="02.php">二月</a></li>
-    <li><a href="03.php">三月</a></li>
-    <li><a href="04.php">四月</a></li>
-    <li><a href="05.php">五月</a></li>
-    <li><a href="06.php">六月</a></li>
-    <li><a href="07.php">七月</a></li>
-    <li><a href="08.php">八月</a></li>
-    <li><a href="09.php">九月</a></li>
-    <li><a href="10.php">十月</a></li>
-    <li><a href="11.php">十一月</a></li>
-    <li><a href="12.php">十二月</a></li>
-</ul>
-<?php 
-echo "<h3>";
-echo date("西元Y年m月");
-echo "</h3>";
-$thisMonth=date("Y");
-$thisFirstDay=date("Y-m-1");
-$thisFirstDate=date('w',strtotime($thisFirstDay));
-$thisMonthDays=date("t");
-$thisLastDay=date("Y-m-$thisMonthDays");
-$weeks=ceil(($thisMonthDays+$thisFirstDate)/7);
-$firstCell=date("Y-m-d",strtotime("-$thisFirstDate days",strtotime($thisFirstDay)));
-echo "<table>";
-echo "<tr>";
-echo "<td style='background-color: #3498db; color: #fff;'>日</td>";
-echo "<td style='background-color: #3498db; color: #fff;'>一</td>";
-echo "<td style='background-color: #3498db; color: #fff;'>二</td>";
-echo "<td style='background-color: #3498db; color: #fff;'>三</td>";
-echo "<td style='background-color: #3498db; color: #fff;'>四</td>";
-echo "<td style='background-color: #3498db; color: #fff;'>五</td>";
-echo "<td style='background-color: #3498db; color: #fff;'>六</td>";
-echo "</tr>";
-for($i=0;$i<$weeks;$i++){
-    echo "<tr>";
-    for($j=0;$j<7;$j++){
-        $addDays=7*$i+$j;
-        $thisCellDate=strtotime("+$addDays days",strtotime($firstCell));
-        if(date('w',$thisCellDate)==0 || date('w',$thisCellDate)==6){
-            echo "<td style='background: pink;'>";
-
-        }else{
-            echo "<td>";
-        }
-        if(date("m",$thisCellDate)==date("m",strtotime($thisFirstDay))){
-            echo date("j",$thisCellDate);
-        }
-        echo "</td>";
+    if (isset($_GET['month']) && isset($_GET['year'])) {
+        $month = $_GET['month'];
+        $year = $_GET['year'];
+    } else {
+        $month = date('m');
+        $year = date("Y");
     }
-    echo "</tr>";
-}
 
-echo "</table>";
-?>    
+    echo "<h3 style='text-align:center'>";
+    echo date("西元{$year}年{$month}月");
+    echo "</h3>";
+    $thisFirstDay = date("{$year}-{$month}-1");
+    $thisFirstDate = date('w', strtotime($thisFirstDay));
+    $thisMonthDays = date("t");
+    $thisLastDay = date("{$year}-{$month}-$thisMonthDays");
+    $weeks = ceil(($thisMonthDays + $thisFirstDate) / 7);
+    $firstCell = date("Y-m-d", strtotime("-$thisFirstDate days", strtotime($thisFirstDay)));
+    ?>
+    <div class="navbar">
+        <?php
+        $nextYear = $year;
+        $prevYear = $year;
+        if (($month + 1) > 12) {
+            $next = 1;
+            $nextYear = $year + 1;
+        } else {
+            $next = $month + 1;
+        }
+
+        if (($month - 1) < 1) {
+            $prev = 12;
+            $prevYear = $year - 1;
+        } else {
+            $prev = $month - 1;
+        }
+
+        ?>
+        <a href="?year=<?= $prevYear; ?>&month=<?= $prev; ?>">上一個月</a>
+        <a href="?year=<?= $nextYear; ?>&month=<?= $next; ?>">下一個月</a>
+    </div>
+    <br>
+    <br>
+    <table>
+        <tr>
+            <td>日</td>
+            <td>一</td>
+            <td>二</td>
+            <td>三</td>
+            <td>四</td>
+            <td>五</td>
+            <td>六</td>
+        </tr>
+        <?php
+        for ($i = 0; $i < $weeks; $i++) {
+            echo "<tr>";
+            for ($j = 0; $j < 7; $j++) {
+                $addDays = 7 * $i + $j;
+                $thisCellDate = strtotime("+$addDays days", strtotime($firstCell));
+                if (date('w', $thisCellDate) == 0 || date('w', $thisCellDate) == 6) {
+                    echo "<td style='background:pink'>";
+                } else {
+                    echo "<td>";
+                }
+                if (date("m", $thisCellDate) == date("m", strtotime($thisFirstDay))) {
+                    echo date("j", $thisCellDate);
+                }
+                echo "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+        ?>
+
 </body>
+
 </html>
