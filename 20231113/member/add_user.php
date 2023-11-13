82f128606032777066c342dad080e988a9f91d2c
HelloCodeman <955khm@gmail.com>
2023-11-13 13:49:27 +0800

    示範表單資料清洗檢查

--- 20231113/member/add_user.php
+++ 20231113/member/add_user.php
@@ -2,9 +2,10 @@
 $dsn = "mysql:host=localhost;
 charset=utf8;dbname=member";
 $pdo = new PDO($dsn, 'root', '');
-
+$acc=htmlspecialchars(trim($_POST['acc']));
+// 資料清洗指令 htmlspecialchars
 $sql = "insert into `users`(`acc`,`pw`,`name`,`email`,`phone`)
-                     values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['phone']}')";
+                     values('{$acc}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['phone']}')";
 
 $pdo->exec($sql);
 
