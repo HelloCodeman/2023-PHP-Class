d05d582569150bf4c64007af3ac942c16670aa59
HelloCodeman <955khm@gmail.com>
2023-11-13 11:04:56 +0800

    使用PHP練習CRUD操作

--- 20231113/pdo.php
+++ 20231113/pdo.php
@@ -1,9 +1,33 @@
 <?php
-$dsn="mysql:host=localhost;charset=utf8;dbname=php_school";
-$pdo=new PDO($dsn,'root','');
+$dsn = "mysql:host=localhost;charset=utf8;dbname=php_school";
+$pdo = new PDO($dsn, 'root', '');
 
+/*
 $sql="select * from students";
 $rows=$pdo->query($sql)->fetchAll();
-echo "<pre>";
-print_r($rows);
-echo "</pre>";
+*/
+
+/* 增加資料表內容
+$sql = "insert into `dept`(`code`,`name`) values('801','會計科');";
+$pdo->query($sql);
+*/
+
+/* 更改資料表內容
+$sql = "update `dept` set `code`='901',`name`='表演藝術科' where `id`='9'";
+$pdo->query($sql);
+*/
+
+/* 刪除資料表內容
+$sql="delete from `dept` where `id`='8'";
+$pdo->query($sql);
+*/
+
+/* 練習:刪除資料表內容
+$sql="delete from `students` where `id`='479'";
+$pdo->query($sql);
+*/
+
+/* 在localhost網頁上呈現出執行的結果筆數
+$sql="delete from `dept` where `id`='8'";
+echo $pdo->exec($sql);  
+*/
