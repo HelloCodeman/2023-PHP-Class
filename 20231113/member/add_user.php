3bda4ba17121e81e599168e2ec1c1baa5d5e8342
HelloCodeman <955khm@gmail.com>
2023-11-17 08:49:38 +0800

    拆出db 連線及session_start在外部的include檔

--- 20231113/member/add_user.php
+++ 20231113/member/add_user.php
@@ -1,8 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost;
-charset=utf8;dbname=member";
-$pdo = new PDO($dsn, 'root', '');
-$acc=htmlspecialchars(trim($_POST['acc']));
+include_once "./include/connect.php";
+
+$acc = htmlspecialchars(trim($_POST['acc']));
 // 資料清洗指令 htmlspecialchars
 $sql = "insert into `users`(`acc`,`pw`,`name`,`email`,`phone`)
                      values('{$acc}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['phone']}')";
