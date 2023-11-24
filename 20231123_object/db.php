9b7f2abc7380e078c14bfdf18bceb85e350e2f65
HelloCodeman <955khm@gmail.com>
2023-11-24 15:11:12 +0800

    修改自訂函式的參數引用為類別內的成員

--- 20231123_object/db.php
+++ 20231123_object/db.php
@@ -1,26 +1,22 @@
 <?php
 date_default_timezone_set("Asia/Taipei");
 session_start();
-
 class DB
 {
     protected $dsn = "mysql:host=localhost;charset=utf8;dbname=php_school";
     protected $pdo;
     protected $table;
-
     public function __construct($table)
     {
         $this->table = $table;
         $this->pdo = new PDO($this->dsn, 'root', '');
     }
 
-
-    function all($table = null, $where = '', $other = '')
+    function all($where = '', $other = '')
     {
-        global $pdo;
-        $sql = "select * from `$table` ";
+        $sql = "select * from `$this->table` ";
 
-        if (isset($table) && !empty($table)) {
+        if (isset($this->table) && !empty($this->table)) {
 
             if (is_array($where)) {
 
@@ -36,17 +32,16 @@ class DB
 
             $sql .= $other;
             //echo 'all=>'.$sql;
-            $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+            $rows = $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
             return $rows;
         } else {
             echo "錯誤:沒有指定的資料表名稱";
         }
     }
 
-    function total($table, $id)
+    function find($id)
     {
-        global $pdo;
-        $sql = "select count(`id`) from `$table` ";
+        $sql = "select * from `$this->table` ";
 
         if (is_array($id)) {
             foreach ($id as $col => $value) {
@@ -59,35 +54,13 @@ class DB
             echo "錯誤:參數的資料型態必須是數字或陣列";
         }
         //echo 'find=>'.$sql;
-        $row = $pdo->query($sql)->fetchColumn();
+        $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
         return $row;
     }
 
-    function find($table, $id)
+    function update($id, $cols)
     {
-        global $pdo;
-        $sql = "select * from `$table` ";
-
-        if (is_array($id)) {
-            foreach ($id as $col => $value) {
-                $tmp[] = "`$col`='$value'";
-            }
-            $sql .= " where " . join(" && ", $tmp);
-        } else if (is_numeric($id)) {
-            $sql .= " where `id`='$id'";
-        } else {
-            echo "錯誤:參數的資料型態必須是數字或陣列";
-        }
-        //echo 'find=>'.$sql;
-        $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
-        return $row;
-    }
-
-    function update($table, $id, $cols)
-    {
-        global $pdo;
-
-        $sql = "update `$table` set ";
+        $sql = "update `$this->table` set ";
 
         if (!empty($cols)) {
             foreach ($cols as $col => $value) {
@@ -110,14 +83,12 @@ class DB
             echo "錯誤:參數的資料型態必須是數字或陣列";
         }
         // echo $sql;
-        return $pdo->exec($sql);
+        return $this->pdo->exec($sql);
     }
 
-    function insert($table, $values)
+    function insert($values)
     {
-        global $pdo;
-
-        $sql = "insert into `$table` ";
+        $sql = "insert into `$this->table` ";
         $cols = "(`" . join("`,`", array_keys($values)) . "`)";
         $vals = "('" . join("','", $values) . "')";
 
@@ -125,13 +96,13 @@ class DB
 
         //echo $sql;
 
-        return $pdo->exec($sql);
+        return $this->pdo->exec($sql);
     }
 
-    function del($table, $id)
+    function del($id)
     {
         global $pdo;
-        $sql = "delete from `$table` where ";
+        $sql = "delete from `$this->$table` where ";
 
         if (is_array($id)) {
             foreach ($id as $col => $value) {
@@ -141,17 +112,21 @@ class DB
         } else if (is_numeric($id)) {
             $sql .= " `id`='$id'";
         } else {
-            echo "錯誤:參數的資料型態比須是數字或陣列";
+            echo "錯誤:參數的資料型態必須是數字或陣列";
         }
         //echo $sql;
 
-        return $pdo->exec($sql);
-    }
-
-    function dd($array)
-    {
-        echo "<pre>";
-        print_r($array);
-        echo "</pre>";
+        return $this->$pdo->exec($sql);
     }
 }
+
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+$student = new DB('students');
+$rows = $student->all();
+dd($rows);
